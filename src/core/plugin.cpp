dcff6d35b6c275126b1f4dd58d875817108f654f
skyeBreach <skyebenson@hotmail.com>
2024-12-11 04:59:13 +0000

    refactor: reorganise directory structure

--- src/core/plugin.cpp
+++ src/core/plugin.cpp
@@ -1,13 +1,13 @@
 // TODO: Fill out your copyright notice and run the 'copyright-update' script
 
-#include "breached-teamspeak/plugin.h"
+#include "breached-teamspeak/core/plugin.h"
+#include "breached-teamspeak/teamspeak.h"
 
 #include "teamspeak/public_definitions.h"
-#include "ts3_functions.h"
 
 #define PATH_BUFSIZE 512
 
-static struct TS3Functions teamspeakFunctions;
+using namespace breached::teamspeak;
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 // PluginBase: Construction/Destruction
@@ -22,7 +22,7 @@ PluginBase::PluginBase(const std::string pluginID, QObject* parent)
 int PluginBase::Initialize()
 {
     // TODO: Convert to internal logging library
-    teamspeakFunctions.logMessage("Plugin: Starting initialization", LogLevel::LogLevel_INFO, pluginID.c_str(), 0);
+    GetTS3Functions().logMessage("Plugin: Starting initialization", LogLevel::LogLevel_INFO, pluginID.c_str(), 0);
 
     // Initial Testing path vars
     char appPath[PATH_BUFSIZE];
@@ -30,14 +30,14 @@ int PluginBase::Initialize()
     char configPath[PATH_BUFSIZE];
     char pluginPath[PATH_BUFSIZE];
 
-    teamspeakFunctions.getAppPath(appPath, PATH_BUFSIZE);
-    teamspeakFunctions.getResourcesPath(resourcesPath, PATH_BUFSIZE);
-    teamspeakFunctions.getConfigPath(configPath, PATH_BUFSIZE);
-    teamspeakFunctions.getPluginPath(pluginPath, PATH_BUFSIZE, pluginID.c_str());
+    GetTS3Functions().getAppPath(appPath, PATH_BUFSIZE);
+    GetTS3Functions().getResourcesPath(resourcesPath, PATH_BUFSIZE);
+    GetTS3Functions().getConfigPath(configPath, PATH_BUFSIZE);
+    GetTS3Functions().getPluginPath(pluginPath, PATH_BUFSIZE, pluginID.c_str());
 
     char buff[100];
     snprintf(buff, sizeof(buff), "PLUGIN: App path: %s\nResources path: %s\nConfig path: %s\nPlugin path: %s\n", appPath, resourcesPath, configPath, pluginPath);
-    teamspeakFunctions.logMessage(
+    GetTS3Functions().logMessage(
         buff, 
         LogLevel_INFO, 
         pluginID.c_str(), 
@@ -45,7 +45,7 @@ int PluginBase::Initialize()
     );
 
     // TODO: Convert to internal logging library
-    teamspeakFunctions.logMessage("Plugin: Successfully initialized plugin", LogLevel::LogLevel_INFO, pluginID.c_str(), 0);
+    GetTS3Functions().logMessage("Plugin: Successfully initialized plugin", LogLevel::LogLevel_INFO, pluginID.c_str(), 0);
     return 0;
 }
 
