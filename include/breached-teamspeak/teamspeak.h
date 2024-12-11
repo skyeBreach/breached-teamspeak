ffd77f47e45d2c64e5d3cc1e5456844a5abf46de
skyeBreach <skyebenson@hotmail.com>
2024-12-11 05:00:16 +0000

    feat: add in initial ts3 functions

---
+++ include/breached-teamspeak/teamspeak.h
@@ -0,0 +1,8 @@
+struct TS3Functions;
+
+namespace breached {
+    namespace teamspeak { 
+        void SetTS3Functions(const struct TS3Functions funcs);
+        TS3Functions GetTS3Functions();
+    }
+}
---
+++ src/teamspeak.cpp
@@ -0,0 +1,20 @@
+#include "breached-teamspeak/teamspeak.h"
+#include "ts3_functions.h"
+
+static struct TS3Functions tsFunctions;
+
+void breached::teamspeak::SetTS3Functions(const struct TS3Functions funcs)
+{
+    // Only set the function ptrs if they have not been set previously
+    if(tsFunctions.getClientLibVersion == nullptr) 
+    {
+        tsFunctions = funcs;
+        return;
+    }
+    // TODO: if so then we need to create a debug warning 
+}
+
+TS3Functions breached::teamspeak::GetTS3Functions()
+{
+    return tsFunctions;
+}
